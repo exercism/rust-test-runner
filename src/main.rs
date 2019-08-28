6b3a59b261eb2af5701e16f7473a2a7fe1753f2e
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-08-28 23:03:50 +0200

    pass through the test output unchanged

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{BufRead, Write};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let stdout = std::io::stdout();
+    let mut stdout_lock = stdout.lock();
+
+    let mut line_buf = String::with_capacity(255);
+    while let Ok(n) = stdin_lock.read_line(&mut line_buf) {
+        if n == 0 {
+            break;
+        }
+        write!(stdout_lock, "{}", line_buf)?;
+        line_buf.clear();
+    }
+    Ok(())
 }
