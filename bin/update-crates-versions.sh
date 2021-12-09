b2e086891b33f504e96a5488d4c2453d9629165c
Denis Hovart <hello@denishovart.com>
2021-12-09 10:47:34 -0300

    Update workflow to follow recs in #40

--- bin/update-crates-versions.sh
+++ bin/update-crates-versions.sh
@@ -8,6 +8,6 @@ while IFS= read -r line
 do
     crate=$(echo "$line" | awk -F"[ ]+" '{print $1}')
     >&2 echo "Retrieving latest version of $crate..."
-    latest_version=$(cargo search "$crate" | head -n 1 | awk -F"[ ]+" '{print $1  " = " $3}')
-    sed -i -r "s/^$crate =.*/$latest_version/" $MANIFEST
+    latest_version=$(cargo search "$crate" | head -n 1 | awk -F"[ ]+" '{print $1  " = " $3}' | sed 's/"/\\"/g')
+    sed -i "s/^$crate =.*/$latest_version/" $MANIFEST
  done <<< "$DEPS"
