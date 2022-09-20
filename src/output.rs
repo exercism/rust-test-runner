c203ad8a852663fc0c95a431e28c28498ad20598
dem4ron <demaaron88@gmail.com>
2022-09-20 21:39:55 +0200

    Add testname formatter fn

--- src/output.rs
+++ src/output.rs
@@ -18,7 +18,7 @@ pub struct TestResult {
 impl TestResult {
     pub fn ok(name: String) -> TestResult {
         TestResult {
-            name,
+            name: format_test_name(name),
             status: Status::Pass,
             message: None,
         }
@@ -26,10 +26,24 @@ impl TestResult {
 
     pub fn fail(name: String, message: Option<String>) -> TestResult {
         TestResult {
-            name,
+            name: format_test_name(name),
             message,
             status: Status::Fail,
         }
+    }
+}
+
+/* Removes "test_" from test-name, replaces underscores with a whitespace, turns it into title case
+*  
+* e.g. test_year_divisible_by_400_but_not_by_125_is_still_a_leap_year -> Year divisible by 400 but not by 125 is still a leap year
+* 
+* Why is this important? See https://github.com/exercism/exercism/issues/6544 */
+fn format_test_name(name: String) -> String {
+    let sanitized_name = name.replace("test_", "").replace("_", " ");
+    let mut c = sanitized_name.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
