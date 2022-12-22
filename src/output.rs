bc0ae455ac3ad9c453e1c50589b4eec514c8bd35
Remo Senekowitsch <contact@remlse.dev>
2022-12-22 13:01:40 +0100

    Fix flaky CI

--- src/output.rs
+++ src/output.rs
@@ -28,7 +28,16 @@ impl TestResult {
     pub fn fail(name: String, message: Option<String>) -> TestResult {
         TestResult {
             name: format_test_name(name),
-            message,
+            message: message.map(|m| {
+                // This note is attached to the error message of only one test case that fails,
+                // but not always the same one. To avoid CI failing unnecessarily, this note
+                // is stripped from all messages.
+                // It's also not useful to students reading the output of the test runner,
+                // as they can't set this environment variable in the test runner themselves.
+                m.trim_end_matches(
+                    "note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace\n"
+                ).to_owned()
+            }),
             status: Status::Fail,
         }
     }
